038d2e648cdce16b37588d35174a5a822f5f1f45
Andre Macedo <andre.macedo@seidor.com.br>
2020-10-27 11:14:06 -0300

    feat: add userSchema

---
+++ src/validation/schema/user.ts
@@ -0,0 +1,11 @@
+import * as yup from 'yup';
+
+export const userSchema = yup.object().shape({
+    name: yup.string().required(),
+    surname: yup.string().required(),
+    age: yup.number().required().min(0).max(130),
+    email: yup.string().email(),
+    website: yup.string().url(),
+    birthday: yup.date(),
+    created_at: yup.date().default(() => new Date()),
+});
